fb2fe4dd688854324c2d68415a10dc365c3df444
WalterOfNone <walterofnone@protonmail.com>
2023-02-20 03:52:50 -0600

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,112 @@
+use png::{ColorType, Decoder};
+use std::fs::File;
+const GAME_HEIGHT: i32 = 240;
+const GAME_WIDTH: i32 = 426;
+
 pub trait Object {
     /// Returns pixels relative to the current camera
-    fn get_pixels(&self, camera: &Camera, mouse_pos: &(i32,i32)) -> Vec<Pixel>;
+    fn get_pixels(&self, camera: &Camera, mouse_pos: &(i32, i32)) -> Vec<Pixel>;
+    fn update(&mut self, camera: &Camera);
+}
 
-    fn update(&mut self);
+pub struct Entity {
+    pub visible: bool,
+    pub collision: bool,
+    pub hitbox_x: f64,
+    pub hitbox_y: f64,
+    pub coord_x: f64,
+    pub coord_y: f64,
+    pub sprite: String,
+    pub sprite_state: u32,
+}
+
+pub struct Image {
+    pub name: String,
+    pub bytes: Vec<u8>,
+    pub sprite_height: u32,
+    pub sprite_width: u32,
+    pub image_width: u32,
+}
+
+impl Image {
+    //Creates a new Image with a given file name, and amount of divions in the spritesheet
+    pub fn new(path: String, sprite_width: u32) -> Self {
+        //Excellent code
+        let path_name = format!("{}", path);
+
+        let decoder = png::Decoder::new(File::open(path_name).unwrap());
+        let mut reader = decoder.read_info().unwrap();
+        let mut buf = vec![0; reader.output_buffer_size()];
+        let info = reader.next_frame(&mut buf).unwrap();
+        let bytes_arr = &buf[..info.buffer_size()];
+
+        let mut bytes = bytes_arr.to_vec();
+
+        flip_pixels_x_axis(&mut bytes, info.width as usize, info.height as usize);
+
+        Self {
+            name: path,
+            bytes: bytes,
+            sprite_height: info.height,
+            image_width: info.width,
+            sprite_width: sprite_width,
+        }
+    }
+}
+
+fn flip_pixels_x_axis(pixels: &mut Vec<u8>, width: usize, height: usize) {
+    for y in 0..height / 2 {
+        for x in 0..width {
+            let top_index = (y * width + x) * 4;
+            let bottom_index = ((height - y - 1) * width + x) * 4;
+            for i in 0..4 {
+                pixels.swap(top_index + i, bottom_index + i);
+            }
+        }
+    }
+}
+
+impl Object for Entity {
+    fn get_pixels(&self, camera: &Camera, mouse_pos: &(i32, i32)) -> Vec<Pixel> {
+        let mut block = Vec::new();
+        let decoder = png::Decoder::new(File::open(&self.sprite).unwrap());
+        let mut reader = decoder.read_info().unwrap();
+        // Allocate the output buffer.
+        let mut buf = vec![0; reader.output_buffer_size()];
+        // Read the next frame. An APNG might contain multiple frames.
+        let info = reader.next_frame(&mut buf).unwrap();
+        // Grab the bytes of the image.
+        let bytes = &buf[..info.buffer_size()];
+
+        //draws sprite
+        let mut pos: usize = 0;
+        for y in 0..info.height {
+            for x in 0..info.width {
+                //if self.coord_x as i32 + x as i32-4 < 426 && self.coord_y as i32 + y as i32- 4 > 0 {
+                block.push(Pixel {
+                    x: self.coord_x as i32 - camera.x + x as i32,
+                    y: -(self.coord_y as i32 - camera.y + y as i32) + info.height as i32 - 1,
+                    rgba: [
+                        bytes[pos * 4 + 0],
+                        bytes[pos * 4 + 1],
+                        bytes[pos * 4 + 2],
+                        bytes[pos * 4 + 3],
+                    ],
+                });
+                pos += 1;
+                //}
+            }
+        }
+        return block;
+    }
+    //TODO: make this actually function
+    fn update(&mut self, camera: &Camera) {
+        if (self.coord_x as i32) < camera.x || (self.coord_x as i32) > camera.x + GAME_WIDTH {
+            self.visible = true;
+        } else {
+            self.visible = true;
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -40,7 +144,6 @@ impl Camera {
 
         if player.coord_x as i32 > 160 {
             self.x = player.coord_x as i32 - 160;
-            
         } else {
             self.x = 0;
         }
@@ -48,38 +151,55 @@ impl Camera {
 }
 
 impl Object for Player {
-    fn get_pixels(&self, camera: &Camera, mouse_pos: &(i32,i32)) -> Vec<Pixel> {
-
+    fn get_pixels(&self, camera: &Camera, mouse_pos: &(i32, i32)) -> Vec<Pixel> {
         let mut block = Vec::new();
+        let decoder = png::Decoder::new(File::open("sprites/steve.png").unwrap());
+        let mut reader = decoder.read_info().unwrap();
+        // Allocate the output buffer.
+        let mut buf = vec![0; reader.output_buffer_size()];
+        // Read the next frame. An APNG might contain multiple frames.
+        let info = reader.next_frame(&mut buf).unwrap();
+        // Grab the bytes of the image.
+        let bytes = &buf[..info.buffer_size()];
 
         //draws player as block
-        for x in 0..9 {
-            for y in 0..9 {
+        let mut pos: usize = 0;
+        for y in 0..9 {
+            for x in 0..9 {
                 //if self.coord_x as i32 + x -4 < 320 && self.coord_y as i32 + y - 4 > 0 {
-                    block.push(Pixel {
-                        x: self.coord_x as i32 - camera.x + x - 4,
-                        y: self.coord_y as i32 - camera.y + y - 4,
-                        rgba: [x as u8, 80, 80, 255],
-                    })
+                block.push(Pixel {
+                    x: self.coord_x as i32 - camera.x + x - 4,
+                    y: self.coord_y as i32 - camera.y + y - 4,
+                    rgba: [
+                        bytes[pos * 4 + 0],
+                        bytes[pos * 4 + 1],
+                        bytes[pos * 4 + 2],
+                        255,
+                    ],
+                });
+                pos += 1;
                 //}
-                
             }
         }
 
         //draws cursor position in green
         block.push(Pixel {
-            x: mouse_pos.0 % 320,
+            x: mouse_pos.0 % GAME_WIDTH,
             y: mouse_pos.1 - 1,
-            rgba: [0,255,0,255],
+            rgba: [0, 255, 0, 255],
         });
 
         //draws grapple spot
-        if self.grappled{
+        if self.grappled {
             let x_relative = self.grapple_loc.0 - camera.x;
             let y_relative = self.grapple_loc.1 - camera.y;
 
             //checks if coordinates are in frame
-            if x_relative > 0 && x_relative < 320 && y_relative < 180 && y_relative > 0 {
+            if x_relative > 0
+                && x_relative < GAME_WIDTH
+                && y_relative < GAME_HEIGHT
+                && y_relative > 0
+            {
                 block.push(Pixel {
                     x: x_relative,
                     y: y_relative,
@@ -96,14 +216,14 @@ impl Object for Player {
 
         if self.grappled {
             y_diff = (self.coord_y - self.grapple_loc.1 as f64) as f64;
-            x_diff = (self.coord_x - self.grapple_loc.0  as f64) as f64;
+            x_diff = (self.coord_x - self.grapple_loc.0 as f64) as f64;
             if x_diff != 0.0 {
-                slope = y_diff/x_diff;
+                slope = y_diff / x_diff;
             }
         }
 
         //angle of the grapple hook relative to the player
-        let mut mouse_angle = (y_diff/x_diff).atan();
+        let mut mouse_angle = (y_diff / x_diff).atan();
 
         //increase the mouse angle by 1 rad to make a full circle
         if x_diff >= 0.0 {
@@ -114,11 +234,12 @@ impl Object for Player {
         let grapple_hook_y = (mouse_angle.sin() * 15.0) as i32 + self.coord_y as i32 - camera.y;
 
         //draws grapple hook
-        if grapple_hook_x > 0 && grapple_hook_x < 320 && grapple_hook_y < 180 && grapple_hook_y > 0 {
+        if grapple_hook_x > 0 && grapple_hook_x < 320 && grapple_hook_y < 180 && grapple_hook_y > 0
+        {
             block.push(Pixel {
                 x: grapple_hook_x as i32,
                 y: grapple_hook_y as i32,
-                rgba: [0,0,255,255],
+                rgba: [0, 0, 255, 255],
             });
         }
 
@@ -133,11 +254,11 @@ impl Object for Player {
                 let rope_x = grapple_hook_x as i32 + x;
                 let rope_y = grapple_hook_y as i32 + (x as f64 * slope) as i32;
 
-                if rope_x > 0 && rope_x < 320 && rope_y < 180 && rope_y > 0 {
+                if rope_x > 0 && rope_x < GAME_WIDTH && rope_y < GAME_HEIGHT && rope_y > 0 {
                     block.push(Pixel {
                         x: rope_x,
                         y: rope_y,
-                        rgba: [0,0,255,255],
+                        rgba: [0, 0, 255, 255],
                     });
                 }
             }
@@ -146,7 +267,7 @@ impl Object for Player {
         return block;
     }
 
-    fn update(&mut self) {
+    fn update(&mut self, camera: &Camera) {
         //gravity and ground
         self.coord_x += self.velocity_x;
         self.coord_y += self.velocity_y;
@@ -171,7 +292,7 @@ impl Object for Player {
 
         if self.grappled {
             let y_diff = self.coord_y - self.grapple_loc.1 as f64;
-            let x_diff = self.coord_x - self.grapple_loc.0  as f64 ;
+            let x_diff = self.coord_x - self.grapple_loc.0 as f64;
             self.velocity_x -= x_diff * 0.0005;
             self.velocity_y -= y_diff * 0.0007;
         }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
 pub mod lib;
-use lib::{Object, Pixel, Camera};
+pub mod worldinit;
+
+use lib::{Camera, Entity, Image, Object, Pixel};
 use log::error;
-use pixels::{Error, Pixels, SurfaceTexture, PixelsBuilder};
+use pixels::wgpu::{PowerPreference, RequestAdapterOptions};
+use pixels::{Error, Pixels, PixelsBuilder, SurfaceTexture};
+use worldinit::load_images;
+use std::collections::HashMap;
+use std::time::Instant;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
+use winit::window::{CursorIcon, Fullscreen};
 use winit_input_helper::WinitInputHelper;
-use pixels::wgpu::{PowerPreference, RequestAdapterOptions};
-use std::time::Instant;
-use winit::window::{Fullscreen, CursorIcon};
+
+const GAME_HEIGHT: usize = 240;
+const GAME_WIDTH: usize = 426;
 
 struct World {
     player_1: lib::Player,
-    mouse_pos: (i32,i32),
+    mouse_pos: (i32, i32),
     camera: lib::Camera,
     frames: u64,
     start_time: Instant,
+    entities: Vec<Entity>,
+    sprites: HashMap<String, Image>,
 }
 
 impl World {
     /// Spawns a basic world with player
     fn new() -> Self {
+        let default_images = load_images();
+        
         Self {
             player_1: lib::Player {
                 health: 100,
@@ -33,44 +44,106 @@ impl World {
                 grappled: false,
                 grapple_loc: (0, 0),
             },
-            camera: lib::Camera {x: 0, y: 0},
+            camera: lib::Camera { x: 0, y: 0 },
             mouse_pos: (160, 90),
             frames: 0,
             start_time: Instant::now(),
+            entities: Vec::new(),
+            sprites: default_images,
         }
+    }
+
+    fn spawn(&mut self, entity: Entity) {
+        self.entities.push(entity)
     }
 
     /// Updates world movement
     fn update(&mut self) {
-        //make sure no frame drops
-        self.frames += 1;
-
-        if self.frames % 10000 == 0 {
-            println!("{} FPS", (10000.0 /self.start_time.elapsed().as_secs_f64()));
+        for entity in self.entities.iter_mut() {
+            entity.update(&self.camera);
         }
 
-        self.player_1.update();
+        self.player_1.update(&self.camera);
         self.camera.update(&self.player_1);
     }
 
     fn draw(&self, frame: &mut [u8]) {
-        
-        let mut pre_buffer: [[Pixel; 180]; 320] = [[Pixel {x: 0, y: 0, rgba: [255,255,255,255]}; 180]; 320];
+        let mut pre_buffer: [[[u8; 4]; GAME_HEIGHT]; GAME_WIDTH] = [[[100, 100, 100, 255]; GAME_HEIGHT]; GAME_WIDTH];
+        //Iterates through all visible entities and places visible pixels on pre_buffer
+        //TODO: multithread
+        for entity in self.entities.iter() {
+            if entity.visible {
+                let x_rel = entity.coord_x as i32 - self.camera.x;
+                let y_rel = entity.coord_y as i32 - self.camera.y;
 
-        // grabs pixels from player's display output and assigns them to the pre_buffer
-        for pixel in self.player_1.get_pixels(&self.camera, &self.mouse_pos).iter() {
-            pre_buffer[pixel.x as usize][pixel.y as usize] = pixel.clone();
+                let mut sprite_start_x = 0;
+                if x_rel < 0 {
+                    sprite_start_x = x_rel.abs();
+                }
+                let mut sprite_end_x = self.sprites[&entity.sprite].sprite_width as i32 - 0;
+                if sprite_end_x + x_rel + 1 > GAME_WIDTH as i32 {
+                    sprite_end_x = self.sprites[&entity.sprite].sprite_width as i32 - 0 - ((sprite_end_x + x_rel) - GAME_WIDTH as i32);
+                }
+                //println!("sprite_end_x: {}", sprite_end_x);
+                let mut sprite_start_y: i32 = 0;
+                if y_rel < 0 {
+                    sprite_start_y = y_rel.abs();
+                }
+                let mut sprite_end_y = self.sprites[&entity.sprite].sprite_height as i32 - 0;
+                if sprite_end_y + y_rel + 1 > GAME_HEIGHT as i32 {
+                    //perhaps change from -1 to - 0 here?
+                    sprite_end_y = self.sprites[&entity.sprite].sprite_height as i32 - 1 - ((sprite_end_y + y_rel) - GAME_HEIGHT as i32);
+                }
+
+                let sprite = &self.sprites[&entity.sprite];
+
+                for x in (sprite_start_x + x_rel)..(sprite_end_x + x_rel) {
+                    for y in (sprite_start_y + y_rel)..(sprite_end_y + y_rel) {
+                        if sprite.bytes[((x - x_rel + sprite.sprite_width as i32 * (y - y_rel)) * 4+ 3) as usize] == 255 {
+                            pre_buffer[x as usize][y as usize][0] = sprite.bytes[((x - x_rel + sprite.sprite_width as i32 * (y - y_rel))* 4+ 0)as usize];
+                            pre_buffer[x as usize][y as usize][1] = sprite.bytes[((x - x_rel + sprite.sprite_width as i32 * (y - y_rel))* 4+ 1)as usize];
+                            pre_buffer[x as usize][y as usize][2] = sprite.bytes[((x - x_rel+ sprite.sprite_width as i32 * (y - y_rel))* 4+ 2)as usize];
+                            pre_buffer[x as usize][y as usize][3] = 255;
+                        } else if sprite.bytes[((x - x_rel+ sprite.sprite_width as i32 * (y - y_rel))* 4+ 3) as usize] == 0 {
+                            //adds no pixel
+                        } else {
+                            let src: &[u8; 4] = &pre_buffer[x as usize][y as usize][0..4].try_into().unwrap();
+                            let dst = &sprite.bytes[((x - x_rel + sprite.sprite_width as i32 * (y - y_rel))* 4+ 0) as usize..((x - x_rel + sprite.sprite_width as i32 * (y - y_rel)) * 4 + 4)as usize].try_into().unwrap();
+                            let blended = blend_alpha_fast(src, dst);
+                            pre_buffer[x as usize][y as usize][0] = blended[0];
+                            pre_buffer[x as usize][y as usize][1] = blended[1];
+                            pre_buffer[x as usize][y as usize][2] = blended[2];
+                            pre_buffer[x as usize][y as usize][3] = blended[3];
+                        }
+                    }
+                }
+            }
         }
 
+        //copies pixel array into current frame
         for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-            let x = (i % 320 as usize) as i16;
-            let y = (i / 320 as usize) as i16;
+            let x = (i % GAME_WIDTH as usize) as i16;
+            let y = (i / GAME_WIDTH as usize) as i16;
 
-            let rgba = pre_buffer[x as usize][179 - y  as usize].rgba;
+            let rgba = pre_buffer[x as usize][GAME_HEIGHT - 1 - y as usize];
 
             pixel.copy_from_slice(&rgba);
         }
     }
+}
+
+//Blends alpha between 2 pixels quickly. Not a correct implementation, as it ignores the background pixel's alpha.
+fn blend_alpha_fast(&src: &[u8; 4], &dst: &[u8; 4]) -> [u8; 4] {
+    let mut blended = [255 as u8; 4];
+    blended[0] = (dst[0] as f64 * (dst[3] as f64) / 255.0) as u8
+        + (src[0] as f64 * (255.0 - dst[3] as f64) / 255.0) as u8;
+    blended[1] = (dst[1] as f64 * (dst[3] as f64) / 255.0) as u8
+        + (src[1] as f64 * (255.0 - dst[3] as f64) / 255.0) as u8;
+    blended[2] = (dst[2] as f64 * (dst[3] as f64) / 255.0) as u8
+        + (src[2] as f64 * (255.0 - dst[3] as f64) / 255.0) as u8;
+    blended[3] = 255;
+
+    return blended;
 }
 
 fn main() -> Result<(), Error> {
@@ -78,8 +151,9 @@ fn main() -> Result<(), Error> {
     let event_loop = EventLoop::new();
     let mut input = WinitInputHelper::new();
 
+    //creates window with specified game width, scales to higher res
     let window = {
-        let min_size = LogicalSize::new(320,180);
+        let min_size = LogicalSize::new(426, 240);
         let size = LogicalSize::new(1920, 1080);
         WindowBuilder::new()
             .with_title("Verified Game Testing Moment")
@@ -94,17 +168,30 @@ fn main() -> Result<(), Error> {
     let window_size = window.inner_size();
     let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
 
-    let mut pixels = PixelsBuilder::new(320, 180, surface_texture)
-    .request_adapter_options(RequestAdapterOptions {
-        power_preference: PowerPreference::HighPerformance,
-        force_fallback_adapter: false,
-        compatible_surface: None,
-    })
-    .enable_vsync(true)
-    .build()?;
+    let mut pixels = PixelsBuilder::new(426, 240, surface_texture)
+        .request_adapter_options(RequestAdapterOptions {
+            power_preference: PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: None,
+        })
+        .enable_vsync(false)
+        .build()?;
 
     let mut world = World::new();
 
+    let steve = Entity {
+        visible: true,
+        collision: false,
+        hitbox_x: 16.0,
+        hitbox_y: 16.0,
+        coord_x: 20.0,
+        coord_y: 20.0,
+        sprite: String::from("steve.png"),
+        sprite_state: 0,
+    };
+
+    world.spawn(steve);
+    
     event_loop.run(move |event, _, control_flow| {
         // Draw the current frame
         if let Event::RedrawRequested(_) = event {
@@ -120,6 +207,7 @@ fn main() -> Result<(), Error> {
         }
 
         // I should probably figure out a better way to do things than this but oh well for now
+        // perhaps separate file to handle these inputs?
         if input.update(&event) {
             // Close events
             if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
@@ -127,17 +215,17 @@ fn main() -> Result<(), Error> {
                 return;
             }
 
-            if input.key_pressed(VirtualKeyCode::W) && world.player_1.grounded{
+            if input.key_pressed(VirtualKeyCode::W) && world.player_1.grounded {
                 world.player_1.velocity_y += 1.5;
                 world.player_1.grounded = false;
             }
 
-            if input.key_held(VirtualKeyCode::S) || input.quit(){
+            if input.key_held(VirtualKeyCode::S) || input.quit() {
                 world.player_1.velocity_y -= 0.1;
             }
 
-            if input.key_held(VirtualKeyCode::A) || input.quit(){
-                if world.player_1.velocity_x <= -0.5 && world.player_1.velocity_x <= 0.0{
+            if input.key_held(VirtualKeyCode::A) || input.quit() {
+                if world.player_1.velocity_x <= -0.5 && world.player_1.velocity_x <= 0.0 {
                     world.player_1.velocity_x = -0.5;
                 } else {
                     world.player_1.velocity_x -= 0.5;
@@ -150,10 +238,10 @@ fn main() -> Result<(), Error> {
                 }
             }
 
-            if input.key_held(VirtualKeyCode::D) || input.quit(){
-                if world.player_1.velocity_x <= 0.5 && world.player_1.velocity_x >= 0.0{
+            if input.key_held(VirtualKeyCode::D) || input.quit() {
+                if world.player_1.velocity_x <= 0.5 && world.player_1.velocity_x >= 0.0 {
                     world.player_1.velocity_x = 0.5;
-                } else if world.player_1.velocity_x <= 0.5{
+                } else if world.player_1.velocity_x <= 0.5 {
                     world.player_1.velocity_x += 0.5;
                 }
             }
@@ -172,17 +260,20 @@ fn main() -> Result<(), Error> {
             let mouse_diff = input.mouse_diff();
             if mouse_diff != (0.0, 0.0) {
                 match input.mouse() {
-                    None => print!("cheese"),
+                    None => print!("Something with the mouse became unholy! Check line 303 for more details! :)"),
                     Some(coord) => {
                         world.mouse_pos.0 = (coord.0 / 6.0) as i32;
-                        world.mouse_pos.1 = 180 - (coord.1 / 6.0) as i32;
-                    },
+                        world.mouse_pos.1 = 240 - (coord.1 / 6.0) as i32;
+                    }
                 }
             }
 
             if input.mouse_pressed(0) {
                 world.player_1.grappled = true;
-                world.player_1.grapple_loc = (world.mouse_pos.0 + world.camera.x, world.mouse_pos.1 + world.camera.y);
+                world.player_1.grapple_loc = (
+                    world.mouse_pos.0 + world.camera.x,
+                    world.mouse_pos.1 + world.camera.y,
+                );
             }
 
             if input.mouse_released(0) {
