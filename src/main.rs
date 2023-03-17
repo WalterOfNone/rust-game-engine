06d8610d63dada5dbd34edc7e53077c7340e64c4
lquattrocchi2 <lquattrocchi2@huskers.unl.edu>
2023-03-17 07:21:28 -0500

    Added temporary physics engine. Preliminary ECS architecture

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 pub mod lib;
 pub mod worldinit;
+pub mod physics;
 
-use lib::{Camera, Entity, Image, Object, Pixel};
+use gametesting::Collider;
+use gametesting::Coordinates;
+use gametesting::Sprite;
+use lib::{Camera, Entity, Image, Object, Pixel, ComponentVec};
 use log::error;
+use physics::simulate_frame;
 use pixels::wgpu::{PowerPreference, RequestAdapterOptions};
-use pixels::{Error, Pixels, PixelsBuilder, SurfaceTexture};
+use pixels::{Error, PixelsBuilder, SurfaceTexture};
 use worldinit::load_images;
+use std::cell::RefCell;
+use std::cell::RefMut;
+use std::cell::Ref;
 use std::collections::HashMap;
 use std::time::Instant;
 use winit::dpi::LogicalSize;
@@ -22,17 +30,18 @@ struct World {
     player_1: lib::Player,
     mouse_pos: (i32, i32),
     camera: lib::Camera,
-    frames: u64,
-    start_time: Instant,
+    last_updated: Instant,
+    renderable_entities: HashMap<i32, i32>, //This is likely not the best data type to be using
     entities: HashMap<i32, Entity>,
     sprites: HashMap<String, Image>,
+    entities_count: usize,
+    component_vecs: Vec<Box<dyn ComponentVec>>,
 }
 
 impl World {
     /// Spawns a basic world with player
     fn new() -> Self {
         let default_images = load_images();
-        
         Self {
             player_1: lib::Player {
                 health: 100,
@@ -46,63 +55,154 @@ impl World {
             },
             camera: lib::Camera { x: 0, y: 0 },
             mouse_pos: (160, 90),
-            frames: 0,
-            start_time: Instant::now(),
+            last_updated: Instant::now(),
+            renderable_entities: HashMap::new(),
             entities: HashMap::new(),
             sprites: default_images,
+            entities_count: 0,
+            component_vecs: Vec::new(),
         }
     }
-
+    
+    fn new_entity(&mut self) -> usize {
+        let entity_id = self.entities_count;
+        for component_vec in self.component_vecs.iter_mut() {
+            component_vec.push_none();
+        }
+        self.entities_count += 1;
+        entity_id
+    }
+    
+    // We've changed the return type to be a `RefMut`. 
+    // That's what `RefCell` returns when `borow_mut` is used to borrow from the `RefCell`
+    // When `RefMut` is dropped the `RefCell` is alerted that it can be borrowed from again.
+    fn borrow_component_vec_mut<ComponentType: 'static>(
+        &self,
+    ) -> Option<RefMut<Vec<Option<ComponentType>>>> {
+        for component_vec in self.component_vecs.iter() {
+            if let Some(component_vec) = component_vec
+                .as_any()
+                .downcast_ref::<RefCell<Vec<Option<ComponentType>>>>()
+            {
+                // Here we use `borrow_mut`. 
+                // If this `RefCell` is already borrowed from this will panic.
+                return Some(component_vec.borrow_mut());
+            }
+        }
+        None
+    }
+    
+    // We've changed the return type to be a `RefMut`. 
+    // That's what `RefCell` returns when `borow_mut` is used to borrow from the `RefCell`
+    // When `RefMut` is dropped the `RefCell` is alerted that it can be borrowed from again.
+    fn borrow_component_vec<ComponentType: 'static>(
+        &self,
+    ) -> Option<Ref<Vec<Option<ComponentType>>>> {
+        for component_vec in self.component_vecs.iter() {
+            if let Some(component_vec) = component_vec
+                .as_any()
+                .downcast_ref::<RefCell<Vec<Option<ComponentType>>>>()
+            {
+                // Here we use `borrow_mut`. 
+                // If this `RefCell` is already borrowed from this will panic.
+                return Some(component_vec.borrow());
+            }
+        }
+        None
+    }
+    
+    fn add_component_to_entity<ComponentType: 'static>(
+        &mut self,
+        entity: usize,
+        component: ComponentType,
+    ) {
+        for component_vec in self.component_vecs.iter_mut() {
+            if let Some(component_vec) = component_vec
+                .as_any_mut()
+                .downcast_mut::<RefCell<Vec<Option<ComponentType>>>>()
+            {
+                component_vec.get_mut()[entity] = Some(component);
+                return;
+            }
+        }
+        // No matching component storage exists yet, so we have to make one.
+        let mut new_component_vec: Vec<Option<ComponentType>> =
+            Vec::with_capacity(self.entities_count);
+    
+        // All existing entities don't have this component, so we give them `None`
+        for _ in 0..self.entities_count {
+            new_component_vec.push(None);
+        }
+    
+        // Give this Entity the Component.
+        new_component_vec[entity] = Some(component);
+        self.component_vecs.push(Box::new(RefCell::new(new_component_vec)));   
+    }
+        
     fn spawn(&mut self, entity: Entity) {
         self.entities.insert(entity.id, entity);
     }
 
     /// Updates world movement
     fn update(&mut self) {
+        self.last_updated = Instant::now();
         for entity in self.entities.iter_mut() {
-            entity.1.update(&self.camera);
+            entity.1.update(&self.camera, &mut self.renderable_entities);
         }
 
-        self.player_1.update(&self.camera);
-        self.camera.update(&self.player_1);
+        self.player_1.update(&self.camera, &mut self.renderable_entities);
+        
+        self.camera.update(&self.entities.get(&0).expect("player gone"));
+        
+        let mut colliders = self.borrow_component_vec_mut::<Collider>().unwrap();
+        let mut coordinates = self.borrow_component_vec_mut::<Coordinates>().unwrap();
+        
+        simulate_frame(&self.last_updated, &mut colliders, &mut coordinates, &self.renderable_entities, &self.camera);
     }
 
     fn draw(&self, frame: &mut [u8]) {
         let mut pre_buffer: [[[u8; 4]; GAME_HEIGHT]; GAME_WIDTH] = [[[100, 100, 100, 255]; GAME_HEIGHT]; GAME_WIDTH];
         //Iterates through all visible entities and places visible pixels on pre_buffer
         //TODO: multithread
-        for entity in self.entities.iter() {
-            if entity.1.visible {
-                let x_rel = entity.1.coord_x as i32 - self.camera.x;
-                let y_rel = entity.1.coord_y as i32 - self.camera.y;
+        let sprites = self.borrow_component_vec::<Sprite>().unwrap();
+        let coordinates = self.borrow_component_vec::<Coordinates>().unwrap();
+        let zip = sprites.iter().zip(coordinates.iter());
+        let iter = zip.filter_map(|(health, name)| Some((health.as_ref()?, name.as_ref()?)));
+        
+        for (sprite, coordinates) in iter
+        {
+            if sprite.visible {
+                let x_rel = coordinates.coord_x as i32 - self.camera.x;
+                let y_rel = coordinates.coord_y as i32 - self.camera.y;
 
                 let mut sprite_start_x = 0;
                 if x_rel < 0 {
                     sprite_start_x = x_rel.abs();
                 }
-                let mut sprite_end_x = self.sprites[&entity.1.sprite].sprite_width as i32 - 0;
+                let mut sprite_end_x = self.sprites[sprite.sprite].sprite_width as i32 - 0;
                 if sprite_end_x + x_rel + 1 > GAME_WIDTH as i32 {
-                    sprite_end_x = self.sprites[&entity.1.sprite].sprite_width as i32 - 0 - ((sprite_end_x + x_rel) - GAME_WIDTH as i32);
+                    sprite_end_x = self.sprites[sprite.sprite].sprite_width as i32 - 0 - ((sprite_end_x + x_rel) - GAME_WIDTH as i32);
                 }
                 //println!("sprite_end_x: {}", sprite_end_x);
                 let mut sprite_start_y: i32 = 0;
                 if y_rel < 0 {
                     sprite_start_y = y_rel.abs();
                 }
-                let mut sprite_end_y = self.sprites[&entity.1.sprite].sprite_height as i32 - 0;
+                let mut sprite_end_y = self.sprites[sprite.sprite].sprite_height as i32 - 0;
                 if sprite_end_y + y_rel + 1 > GAME_HEIGHT as i32 {
                     //perhaps change from -1 to - 0 here?
-                    sprite_end_y = self.sprites[&entity.1.sprite].sprite_height as i32 - 1 - ((sprite_end_y + y_rel) - GAME_HEIGHT as i32);
+                    sprite_end_y = self.sprites[sprite.sprite].sprite_height as i32 - 1 - ((sprite_end_y + y_rel) - GAME_HEIGHT as i32);
                 }
 
-                let sprite = &self.sprites[&entity.1.sprite];
+                let sprite = &self.sprites[sprite.sprite];
 
                 for x in (sprite_start_x + x_rel)..(sprite_end_x + x_rel) {
                     for y in (sprite_start_y + y_rel)..(sprite_end_y + y_rel) {
                         if sprite.bytes[((x - x_rel + sprite.sprite_width as i32 * (y - y_rel)) * 4+ 3) as usize] == 255 {
-                            pre_buffer[x as usize][y as usize][0] = sprite.bytes[((x - x_rel + sprite.sprite_width as i32 * (y - y_rel))* 4+ 0)as usize];
-                            pre_buffer[x as usize][y as usize][1] = sprite.bytes[((x - x_rel + sprite.sprite_width as i32 * (y - y_rel))* 4+ 1)as usize];
-                            pre_buffer[x as usize][y as usize][2] = sprite.bytes[((x - x_rel+ sprite.sprite_width as i32 * (y - y_rel))* 4+ 2)as usize];
+                            //perhaps calc pixel location once, then add 1,2,3 for rgb value later?
+                            pre_buffer[x as usize][y as usize][0] = sprite.bytes[((x - x_rel + sprite.sprite_width as i32 * (y - y_rel))* 4 + 0)as usize];
+                            pre_buffer[x as usize][y as usize][1] = sprite.bytes[((x - x_rel + sprite.sprite_width as i32 * (y - y_rel))* 4 + 1)as usize];
+                            pre_buffer[x as usize][y as usize][2] = sprite.bytes[((x - x_rel+ sprite.sprite_width as i32 * (y - y_rel))* 4 + 2)as usize];
                             pre_buffer[x as usize][y as usize][3] = 255;
                         } else if sprite.bytes[((x - x_rel+ sprite.sprite_width as i32 * (y - y_rel))* 4+ 3) as usize] == 0 {
                             //adds no pixel
@@ -132,7 +232,7 @@ impl World {
     }
 }
 
-//Blends alpha between 2 pixels quickly. Not a correct implementation, as it ignores the background pixel's alpha.
+// Blends alpha between 2 pixels quickly. Not a correct implementation, as it ignores the background pixel's alpha.
 fn blend_alpha_fast(&src: &[u8; 4], &dst: &[u8; 4]) -> [u8; 4] {
     let mut blended = [255 as u8; 4];
     blended[0] = (dst[0] as f64 * (dst[3] as f64) / 255.0) as u8 + (src[0] as f64 * (255.0 - dst[3] as f64) / 255.0) as u8;
@@ -182,12 +282,84 @@ fn main() -> Result<(), Error> {
         collision: false,
         hitbox_x: 16.0,
         hitbox_y: 16.0,
-        coord_x: 20.0,
-        coord_y: 20.0,
+        coord_x: 0.0,
+        coord_y: 0.0,
         sprite: String::from("steve.png"),
         sprite_state: 0,
     };
-
+    
+    world.new_entity();
+    world.add_component_to_entity(0, Sprite {
+        visible: true,
+        sprite: "coord.png",
+        sprite_state: 0,
+    });
+    world.add_component_to_entity(0, Coordinates { 
+        coord_x: 50.0,
+        coord_y: 50.0});
+    world.add_component_to_entity(0, Collider {
+        rigid_body: true,
+        active: true,
+        collision: true,
+        boundary: (0.0, 0.0, 16.0, 16.0),
+        vel_x: 0.0,
+        vel_y: 0.15,
+    });
+        
+    world.new_entity();
+    world.add_component_to_entity(1, Sprite {
+        visible: true,
+        sprite: "coord.png",
+        sprite_state: 0,
+    });
+    world.add_component_to_entity(1, Coordinates { 
+        coord_x: 50.0,
+        coord_y: 110.0});
+    world.add_component_to_entity(1, Collider {
+        rigid_body: true,
+        active: true,
+        collision: true,
+        boundary: (0.0, 0.0, 16.0, 16.0),
+        vel_x: 0.00,
+        vel_y: 0.00,
+    });
+        
+    world.new_entity();
+    world.add_component_to_entity(2, Sprite {
+        visible: true,
+        sprite: "coord.png",
+        sprite_state: 0,
+    });
+    world.add_component_to_entity(2, Coordinates { 
+        coord_x: 100.0,
+        coord_y: 50.0});
+    world.add_component_to_entity(2, Collider {
+        rigid_body: true,
+        active: true,
+        collision: true,
+        boundary: (0.0, 0.0, 16.0, 16.0),
+        vel_x: 0.00,
+        vel_y: 0.00,
+    });
+        
+    world.new_entity();
+    world.add_component_to_entity(3, Sprite {
+        visible: true,
+        sprite: "steve.png",
+        sprite_state: 0,
+    });
+    world.add_component_to_entity(3, Coordinates { 
+        coord_x: 0.0,
+        coord_y: 0.0});
+    world.add_component_to_entity(3, Collider {
+        rigid_body: true,
+        active: true,
+        collision: true,
+        boundary: (0.0, 0.0, 426.0, 4.0),
+        vel_x: 0.00,
+        vel_y: 0.00,
+    });
+        
     world.spawn(player);
     
     event_loop.run(move |event, _, control_flow| {
