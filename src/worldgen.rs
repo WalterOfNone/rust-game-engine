712099926db480006e4d77e4f572d52b600dd729
lquattrocchi2 <lquattrocchi2@huskers.unl.edu>
2023-05-17 00:25:33 -0500

    cleanup

--- src/worldgen.rs
+++ src/worldgen.rs
@@ -1,12 +1,9 @@
 use rand::prelude::*;
 use rand::rngs::SmallRng;
-use std::arch::x86_64::_SIDD_MASKED_POSITIVE_POLARITY;
-use std::{thread, time};
-use crate::display_tiles;
 use std::collections::{BinaryHeap, HashMap, HashSet};
-use std::cmp::Reverse;
 
 #[derive(Clone, Debug)]
+// Tile that represents a room. 0 Indicates an open pathway for a given direction.
 pub struct Tile {
     pub up: u8,
     pub down: u8,
@@ -15,35 +12,36 @@ pub struct Tile {
 }
 
 // Generates a tiled maze
-pub fn gen_maze(seed: &mut u64, height: usize, width: usize, rand_tiles: f64) -> Vec<Vec<Option<Tile>>>{
+pub fn gen_maze(seed: &mut u64, height: usize, width: usize, rand_level: f64) -> Vec<Vec<Option<Tile>>>{
     let mut tiles = vec![vec![None; width]; height];
     let mut active: Vec<(usize, usize)> = vec![(0,0)];
 
     tiles[0][0] = Some(Tile { up: 0, down: 0, left: 0, right: 0});
 
     while !active.is_empty() {
-        //display_tiles(&tiles).unwrap();
-        //let ten_millis = time::Duration::from_millis(7);
-        //thread::sleep(ten_millis);
         let mut rng = SmallRng::seed_from_u64(seed.clone());
         *seed += 1;
 
-        let num = rng.gen_range(0.0..1.0);
-        let mut active_index = active.len() - 1 ;
-        if num < rand_tiles {
+        // The active tile is chosen from a random tile or from the last active tile
+        // rand_level is the % chance a random tile is chosen
+        let active_index = if rand_level > rng.gen_range(0.0..1.0) {
             rng = SmallRng::seed_from_u64(seed.clone());
             *seed += 1;
-            active_index = rng.gen_range(0..active.len());
-        }
+            rng.gen_range(0..active.len())
+        } else {
+            active.len() - 1
+        };
         let active_pos = active[active_index];
-
+        
         let empty_tiles = get_adjacent_empty(active_pos, &tiles);
         if !empty_tiles.is_empty() {
             *seed += 1;
-            let mut rng = SmallRng::seed_from_u64(seed.clone());
+            rng = SmallRng::seed_from_u64(seed.clone());
+            // Neighboring empty tile is chosen at random
             let empty_tile_index = rng.gen_range(0..empty_tiles.len());
             let empty_tile_location = empty_tiles[empty_tile_index];
 
+            // The empty tile is assigned a tile, and a path is made to it
             match empty_tile_location {
                 (x, y) if x < active_pos.0 => {
                     tiles[active_pos.0][active_pos.1].as_mut().unwrap().left = 1;
@@ -67,7 +65,7 @@ pub fn gen_maze(seed: &mut u64, height: usize, width: usize, rand_tiles: f64) ->
     return tiles;
 }
 
-// Returns a list of positions for the tiles empty and adjacent to the given tile
+// Returns a list of positions for the tiles empty and adjacent to the given tile //refactor this
 fn get_adjacent_empty(tilepos: (usize, usize), tiles: &Vec<Vec<Option<Tile>>>) -> Vec<(usize, usize)>{
     let mut tiles_adjacent = vec![
         (tilepos.0 + 1, tilepos.1),
@@ -134,30 +132,38 @@ fn reconstruct_path(came_from: &HashMap<(usize, usize), (usize, usize)>, end: (u
 
 // Paths maze using A* algorithm
 pub fn path_maze(maze: &Vec<Vec<Option<Tile>>>, start: &(usize, usize), end: &(usize, usize)) -> Vec<(usize, usize)> {
+    // Creates open set to manage currently open nodes, open_set_set is to manage which nodes are in such a set
     let mut open_set: BinaryHeap<(usize, (usize, usize))> = BinaryHeap::new();
     let mut open_set_set: HashSet<(usize, usize)> = HashSet::new();
     open_set.push((0, *start));
     open_set_set.insert(*start);
     
+    // Map of nodes to their previous node
     let mut came_from: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
     
+    // Map of costs to reach a given node
     let mut g_score: HashMap<(usize, usize), usize> = HashMap::new();
     g_score.insert(*start, 0);
     
+    // Map of (g_score + heuristic estimate) - used to determine which node it picks
     let mut f_score = HashMap::new();
     f_score.insert(*start, manhattan_dist(&start, &end));
     
+    // Picks open node with smallest f_score
     while let Some((_, current)) = open_set.pop() {
         if current == *end {
             return reconstruct_path(&came_from, current);
         }
         
         for accessible in get_adjacent_accessible(&current, maze) {
+            // Sets tentative_g_score to distance from starting node to neighbor through the current node
             let tentative_g_score = g_score.get(&current).unwrap_or_else(|| &usize::MAX) + 1;
-            
+            // With a lower g_score, the g_score + f_score + came_from map is updated to reflect the shortest path to that neighbor
             if tentative_g_score < *g_score.get(&accessible).unwrap_or_else(|| &usize::MAX) {
+                
                 came_from.insert(accessible, current);
                 g_score.insert(accessible, tentative_g_score);
+                
                 let heuristic = manhattan_dist(&accessible, end);
                 f_score.insert(accessible, tentative_g_score + heuristic);
 
@@ -168,6 +174,6 @@ pub fn path_maze(maze: &Vec<Vec<Option<Tile>>>, start: &(usize, usize), end: &(u
             }
         }
     }
-    
+    //Returns blank vector if no path found
     return Vec::new();
 }
